2a0d3e989f079ae40434a8ada0134175763fb483
Matthieu Wipliez <matthieu.wipliez@synflow.com>
2013-09-20 16:44:03 +0200

    added top for SHA step 1 (easier to compare everything)

--- SHA256/vhdl-gen/com/synflow/sha256/step1/TopShaStep1.vhd
+++ SHA256/vhdl-gen/com/synflow/sha256/step1/TopShaStep1.vhd
@@ -1,8 +1,8 @@
 -------------------------------------------------------------------------------
--- Title      : Generated from com.synflow.sha256.step1.TestShaStep1 by Synflow Studio
+-- Title      : Generated from com.synflow.sha256.step1.TopShaStep1 by Synflow Studio
 -- Project    : SHA-256
 -------------------------------------------------------------------------------
--- File       : TestShaStep1.vhd
+-- File       : TopShaStep1.vhd
 -- Author     : Matthieu
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -15,26 +15,28 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 ------------------------------------------------------------------------------
-entity TestShaStep1 is
+entity TopShaStep1 is
   port (
                                           -- Clock signal
   clock    : in  std_logic;
 
-  reset_n  : in  std_logic);
-end TestShaStep1;
+  reset_n  : in  std_logic;
+  msg       : in std_logic_vector(31 downto 0);
+  msg_send  : in std_logic;
+  hash       : out std_logic_vector(255 downto 0);
+  hash_send  : out std_logic);
+end TopShaStep1;
 
 ------------------------------------------------------------------------------
-architecture rtl_TestShaStep1 of TestShaStep1 is
+architecture rtl_TopShaStep1 of TopShaStep1 is
 
   ---------------------------------------------------------------------------
   -- Signals declaration
   ---------------------------------------------------------------------------
-  -- Module : source
-  signal source_stimulus      : std_logic_vector(31 downto 0);
-  signal source_stimulus_send : std_logic;
-  -- Module : topShaStep1
-  signal topShaStep1_hash      : std_logic_vector(255 downto 0);
-  signal topShaStep1_hash_send : std_logic;
+  -- Module : sHA256_step1
+  signal sHA256_step1_Kaddr      : std_logic_vector(5 downto 0);
+  -- Module : romK
+  signal romK_dout      : std_logic_vector(31 downto 0);
   ---------------------------------------------------------------------------
 
 begin
@@ -42,31 +44,25 @@ begin
   ---------------------------------------------------------------------------
   -- Actors and Networks 
   ---------------------------------------------------------------------------
-  source : entity work.Source
+  sHA256_step1 : entity work.SHA256_step1
   port map (
     clock   => clock,
     reset_n => reset_n,
-    stimulus      => source_stimulus,
-    stimulus_send => source_stimulus_send
+    Kin      => romK_dout,
+    msg      => msg,
+    msg_send => msg_send,
+    hash      => hash,
+    hash_send => hash_send,
+    Kaddr      => sHA256_step1_Kaddr
   );
   
-  expected : entity work.Expected
+  romK : entity work.RomK
   port map (
     clock   => clock,
     reset_n => reset_n,
-    hash      => topShaStep1_hash,
-    hash_send => topShaStep1_hash_send
-  );
-  
-  topShaStep1 : entity work.TopShaStep1
-  port map (
-    clock   => clock,
-    reset_n => reset_n,
-    msg      => source_stimulus,
-    msg_send => source_stimulus_send,
-    hash      => topShaStep1_hash,
-    hash_send => topShaStep1_hash_send
+    addr      => sHA256_step1_Kaddr,
+    dout      => romK_dout
   );
 
 
-end architecture rtl_TestShaStep1;
+end architecture rtl_TopShaStep1;
